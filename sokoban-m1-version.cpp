2afc0843a96e0b25220859286bb2e677f8148270
M_THUROW <117399401+MatheusThurow@users.noreply.github.com>
2023-05-06 15:49:58 -0300

    Update sokoban-m1-version.cpp

--- sokoban-m1-version.cpp
+++ sokoban-m1-version.cpp
@@ -23,12 +23,157 @@ void ComandoProfesor()
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+void imprimeMapaPersonagem(char matrizJogo[11][11], int x, int y)
+{
+    /// Imprime o jogo: mapa e personagem.
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 11; j++)
+        {
+            if (i == x && j == y)
+            {
+
+                cout << char(2); // personagem
+            }
+            else
+            {
+                switch (matrizJogo[i][j])
+                {
+                case 0:
+
+                    cout << " ";
+                    break; // caminho
+                case 1:
+
+                    cout << char(219);
+                    break; // parede
+                case 2:
+
+                    cout << char(176);
+                    break; // caixa em posição livre(movimentando)
+                           // default: cout<<"-"; //erro
+                case 3:
+
+                    cout << char(248);
+                    break; // posição final
+
+                case 4:
+
+                    cout << char(178);
+                    break; // caixa em posição final
+                }          // fim switch
+            }
+        }
+        cout << "\n";
+    } // fim for mapa
+}
+
+void movimento(char tecla, char matrizJogo[11][11], int &x, int &y)
+{
+    int modificadorX = 0, modificadorY = 0; // modifcadores de x e y da matriz
+
+    bool ganhou = false;
+
+    if (_kbhit()) // determinar se a tecla foi utilizada ou não
+    {
+        tecla = getch(); // funcionamento tecla
+    }
+
+    switch (tecla)
+    {
+    case 72:
+    case 'w':              // cima
+        modificadorX = -1; // linha x sobe
+        modificadorY = 0;  // linha y permanece
+        break;
+    case 80:
+    case 's':             // baixo
+        modificadorX = 1; // linha x desce
+        modificadorY = 0; // permanece
+        break;
+    case 75:
+    case 'a':              // esquerda
+        modificadorX = 0;  // permanece
+        modificadorY = -1; // vai para esquerda
+        break;
+    case 77:
+    case 'd':             // direita
+        modificadorX = 0; // permanece
+        modificadorY = 1; // direta
+        break;
+    }
+
+    switch (matrizJogo[x + modificadorX][y + modificadorY])
+    {
+    case 0:
+        if (modificadorX != 0)
+            x += modificadorX;
+        if (modificadorY != 0)
+            y += modificadorY;
+        break;
+
+    case 2:
+        switch (matrizJogo[x + modificadorX * 2][y + modificadorY * 2]) // acima da caixa
+        {
+        case 0:
+            matrizJogo[x + modificadorX][y + modificadorY] = 0;
+            matrizJogo[x + modificadorX * 2][y + modificadorY * 2] = 2;
+            if (modificadorX != 0)
+                x += modificadorX;
+            if (modificadorY != 0)
+                y += modificadorY;
+            break;
+
+        case 3:
+            matrizJogo[x + modificadorX][y + modificadorY] = 0;
+            matrizJogo[x + modificadorX * 2][y + modificadorY * 2] = 4;
+            if (modificadorX != 0)
+                x += modificadorX;
+            if (modificadorY != 0)
+                y += modificadorY;
+            break;
+        }
+        break;
+
+    case 3:
+        if (modificadorX != 0)
+            x += modificadorX;
+        if (modificadorY != 0)
+            y += modificadorY;
+        break;
+
+    case 4:
+        switch (matrizJogo[x + modificadorX * 2][y + modificadorY * 2])
+        {
+        case 0:
+            matrizJogo[x + modificadorX][y + modificadorY] = 4;
+            matrizJogo[x + modificadorX * 2][y + modificadorY * 2] = 2;
+            if (modificadorX != 0)
+                x += modificadorX;
+            if (modificadorY != 0)
+                y += modificadorY;
+            break;
+
+        case 3:
+            matrizJogo[x + modificadorX][y + modificadorY] = 3;
+            matrizJogo[x + modificadorX * 2][y + modificadorY * 2] = 4;
+            if (modificadorX != 0)
+                x += modificadorX;
+            if (modificadorY != 0)
+                y += modificadorY;
+
+
+            break;  
+        }
+        break;
+    }
+}
+
 void escolheMatriz(char matrizJogo[11][11], int escolhaMapa, int &x, int &y)
 {
-
     char mapa1[11][11] = {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, // microban|| 10
                           1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, /* posição inicial: x=4,y=5 */
-                          1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0,  
+                          1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0,
                           1, 2, 2, 2, 0, 0, 1, 0, 0, 0, 0,
                           1, 3, 3, 3, 0, 0, 1, 0, 0, 0, 0,
                           1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
@@ -52,7 +197,7 @@ void escolheMatriz(char matrizJogo[11][11], int escolhaMapa, int &x, int &y)
 
     char mapa3[11][11] = {0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, // sasquatch 1
                           0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, /* posição inicial: x=4,y=4 */
-                          0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 
+                          0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1,
                           1, 1, 0, 2, 0, 0, 0, 0, 0, 0, 1,
                           1, 0, 0, 0, 0, 2, 0, 1, 0, 0, 1,
                           1, 1, 1, 0, 2, 1, 1, 1, 0, 0, 1,
@@ -99,155 +244,8 @@ void escolheMatriz(char matrizJogo[11][11], int escolhaMapa, int &x, int &y)
         }
         x = 4;
         y = 4;
-        break;
+        break; 
     }
-}
-
-void imprimeMapaPersonagem(char matrizJogo[11][11], int x, int y)
-{
-    /// Imprime o jogo: mapa e personagem.
-    for (int i = 0; i < 11; i++)
-    {
-        for (int j = 0; j < 11; j++)
-        {
-            if (i == x && j == y)
-            {
-
-                cout << char(2); // personagem
-            }
-            else
-            {
-                switch (matrizJogo[i][j])
-                {
-                case 0:
-
-                    cout << " ";
-                    break; // caminho
-                case 1:
-
-                    cout << char(219);
-                    break; // parede
-                case 2:
-
-                    cout << char(176);
-                    break; // caixa em posição livre(movimentando)
-                           // default: cout<<"-"; //erro
-                case 3:
-
-                    cout << char(248);
-                    break; // posição final
-
-                case 4:
-
-                    cout << char(178);
-                    break; // caixa em posição final
-                }		   // fim switch
-            }
-        }
-        cout << "\n";
-    } // fim for mapa
-}
-
-void movimento(char tecla, char matrizJogo[11][11], int &x, int &y)
-{
-    int modificadorX = 0, modificadorY = 0; // modifcadores de x e y da matriz
-
-    if (_kbhit()) // determinar se a tecla foi utilizada ou não
-    {
-        tecla = getch(); // funcionamento tecla
-    }
-
-    switch (tecla)
-    {
-    case 72:
-    case 'w':			   // cima
-        modificadorX = -1; // linha x sobe
-        modificadorY = 0;  // linha y permanece
-        break;
-    case 80:
-    case 's':			  // baixo
-        modificadorX = 1; // linha x desce
-        modificadorY = 0; // permanece
-        break;
-    case 75:
-    case 'a':			   // esquerda
-        modificadorX = 0;  // permanece
-        modificadorY = -1; // vai para esquerda
-        break;
-    case 77:
-    case 'd':			  // direita
-        modificadorX = 0; // permanece
-        modificadorY = 1; // direta
-        break;
-    }
-
-    switch (matrizJogo[x + modificadorX][y + modificadorY])
-    {
-    case 0:
-        if (modificadorX != 0)
-            x += modificadorX;
-        if (modificadorY != 0)
-            y += modificadorY;
-        break;
-
-    case 2:
-        switch (matrizJogo[x + modificadorX * 2][y + modificadorY * 2]) // acima da caixa
-        {
-        case 0:
-            matrizJogo[x + modificadorX][y + modificadorY] = 0;
-            matrizJogo[x + modificadorX * 2][y + modificadorY * 2] = 2;
-            if (modificadorX != 0)
-                x += modificadorX;
-            if (modificadorY != 0)
-                y += modificadorY;
-            break;
-
-        case 3:
-            matrizJogo[x + modificadorX][y + modificadorY] = 0;
-            matrizJogo[x + modificadorX * 2][y + modificadorY * 2] = 4;
-            if (modificadorX != 0)
-            x += modificadorX;
-            if (modificadorY != 0)
-                y += modificadorY;
-            break;
-        }
-        break;
-
-    case 3:
-        if (modificadorX != 0)
-            x += modificadorX;
-        if (modificadorY != 0)
-            y += modificadorY;
-        break;
-
-    case 4:
-        switch (matrizJogo[x + modificadorX * 2][y + modificadorY * 2])
-        {
-        case 0:
-            matrizJogo[x + modificadorX][y + modificadorY] = 4;
-            matrizJogo[x + modificadorX * 2][y + modificadorY * 2] = 2;
-            if (modificadorX != 0)
-                x += modificadorX;
-            if (modificadorY != 0)
-                y += modificadorY;
-            break;
-
-        case 3:
-            matrizJogo[x + modificadorX][y + modificadorY] = 3;
-            matrizJogo[x + modificadorX * 2][y + modificadorY * 2] = 4;
-            if (modificadorX != 0)
-                x += modificadorX;
-            if (modificadorY != 0)
-                y += modificadorY;
-            break;
-        }
-        break;
-    }
-}
-void PausarLimpar()
-{
-    system("pause");
-    system("cls");
 }
 
 void Dificuldade(char matrizJogo[11][11], int escolha, int &x, int &y)
@@ -259,6 +257,9 @@ void Dificuldade(char matrizJogo[11][11], int escolha, int &x, int &y)
     cout << "Escolha a Dificuldade do jogo: ";
     cin >> escolha;
     escolheMatriz(matrizJogo, escolha, x, y);
+    if(escolha != 1 && 2 && 3){
+        return ;
+    }
 }
 
 void jogar()
@@ -282,47 +283,13 @@ void jogar()
     }
 }
 
-void Sobre()
+void PausarLimpar()
 {
-
-    cout << endl;
-
-    cout << "OQUE E O SOKOBAN?" << endl;
-    cout << " E um tipo de jogo de transporte e movimentação de cubos ou engradados em um armazem. " << endl;
-    cout << " O objetivo e pegar e estocar o engradado em determinada posicoes." << endl;
-    cout << " O Sokoban originalfoi criado em 1981 por Hiroyuki Imabayashi, e publicado em 1982 por Thinking Rabbit," << endl;
-    cout << " uma empresa de software localizada em Takarazuka." << endl;
-
-    cout << endl;
-    cout << endl;
-
-    cout << "REGRAS:" << endl;
-    cout << "O caramujo empurra caixas em torno de um labirinto e tenta coloca-los em locais designados. " << endl;
-    cout << "Pressione a tecla de direção das casas adjacentes para empurrá-los. " << endl;
-    cout << "Pressione 4 para revogar," << endl;
-    cout << "pressione A para mover para cima," << endl;
-    cout << "pressione V para mover para baixo," << endl;
-    cout << "pressione <-- para mover para a esquerda, " << endl;
-    cout << "pressione --> para mover para a direita, " << endl;
-
-    cout << endl;
-
-    cout << " EM D E S E N V O L V I M E N T O " << endl;
-
-    cout << endl;
-    cout << endl;
-
-    cout << "EQUIPE DE DESENVOLVIMENTO:" << endl;
-
-    cout << endl;
-
-    cout << "Matheus Thurow" << endl;
-    cout << "Rodrigo Nicola" << endl;
-    cout << "kaio Saldanha" << endl;
-    cout << "Professor: ---" << endl;
+    system("pause");
+    system("cls");
 }
 
-int main()
+void menu()
 {
 
     int EscolhaMenu;
@@ -344,7 +311,7 @@ int main()
         cout << "x                     3~SAIR~                     x" << endl;
         cout << "x                                                 x" << endl;
         cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
-          cin >> EscolhaMenu;
+        cin >> EscolhaMenu;
 
         system("cls");
 
@@ -360,11 +327,33 @@ int main()
 
         case 2:
 
-            Sobre();
-
-            cout << endl;
-
-            PausarLimpar();
+        cout << endl;
+        cout << "OQUE E O SOKOBAN?" << endl;
+        cout << " E um tipo de jogo de transporte e movimentação de cubos ou engradados em um armazem. " << endl;
+        cout << " O objetivo e pegar e estocar o engradado em determinada posicoes." << endl;
+        cout << " O Sokoban originalfoi criado em 1981 por Hiroyuki Imabayashi, e publicado em 1982 por Thinking Rabbit," << endl;
+        cout << " uma empresa de software localizada em Takarazuka." << endl;
+        cout << endl;
+        cout << endl;
+        cout << "REGRAS:" << endl;
+        cout << "O caramujo empurra caixas em torno de um labirinto e tenta coloca-los em locais designados. " << endl;
+        cout << "Pressione a tecla de direção das casas adjacentes para empurrá-los. " << endl;
+        cout << "Pressione 4 para revogar," << endl;
+        cout << "pressione A para mover para cima," << endl;
+        cout << "pressione V para mover para baixo," << endl;
+        cout << "pressione <-- para mover para a esquerda, " << endl;
+        cout << "pressione --> para mover para a direita, " << endl;
+        cout << endl;
+        cout << " EM D E S E N V O L V I M E N T O " << endl;
+        cout << endl;
+        cout << endl;
+        cout << "EQUIPE DE DESENVOLVIMENTO:" << endl;
+        cout << endl;
+        cout << "Matheus Thurow" << endl;
+        cout << "Rodrigo Nicola" << endl;
+        cout << "kaio Saldanha" << endl;
+        cout << "Professor: ---" << endl;
+        PausarLimpar();
 
             break;
 
@@ -384,7 +373,41 @@ int main()
 
             break;
         }
-    } while (EscolhaMenu != 4);
+    } while (EscolhaMenu != 3);
 
+    
+}
+void voltarMenu(){
+    int v;
+    switch (v)
+    {
+    case 9:
+        return menu(); 
+        break;
+    
+    default:
+        break;
+    }
+}
+
+int main(){
+
+    int usuario;
+
+    cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
+    cout << "x                                                 x" << endl;
+    cout << "x                                                 x" << endl;
+    cout << "x                                                 x" << endl;
+    cout << "x                                                 x" << endl;
+    cout << "x                 S O K O B A N                   x" << endl;
+    cout << "x                                                 x" << endl;
+    cout << "x                                                 x" << endl;
+    cout << "x                                                 x" << endl;
+    cout << "x                                                 x" << endl;
+    cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
+        PausarLimpar();
+        menu();
+   
     return 0;
+
 }
